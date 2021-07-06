72ee3fdfe61a0e401c10c3f72400d157d0724a25
Martin Cho <zoveloper@gmail.com>
2021-07-05 23:31:44 -0400

    JS values and variables

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+
+// let js = 'amazing';
+// console.log(40+8+22);
+
+// console.log('Jonas');
+// console.log(js);
+
+
+// let $function =27;
+// let FirstName = "Jonas";
+// let PI = 3.1415;
+
+// let myFirstJob ='Programming';
+// let myCurrentJob='Teacher';
+
+// let job1='programmer';
+// let ojb2 = 'teacher';
+
+// //////
+
+let country = 'South Korea';
+let continent = 'Asia'
+let population = '50 millions'
+
+console.log(country)
+console.log(continent)
+console.log(population)
