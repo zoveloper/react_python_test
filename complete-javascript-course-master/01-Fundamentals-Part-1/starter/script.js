bcee8403ef85aab5325be5247f49eec8cd3dd99d
Martin Cho <zoveloper@gmail.com>
2021-07-07 22:47:15 -0400

    JS Type Conversion and Coercion && Boolean Logic and Logical Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,51 +1,51 @@
 
-// // let js = 'amazing';
-// // console.log(40+8+22);
+// let js = 'amazing';
+// console.log(40+8+22);
 
-// // console.log('Jonas');
-// // console.log(js);
+// console.log('Jonas');
+// console.log(js);
 
 
-// // let $function =27;
-// // let FirstName = "Jonas";
-// // let PI = 3.1415;
+// let $function =27;
+// let FirstName = "Jonas";
+// let PI = 3.1415;
 
-// // let myFirstJob ='Programming';
-// // let myCurrentJob='Teacher';
+// let myFirstJob ='Programming';
+// let myCurrentJob='Teacher';
 
-// // let job1='programmer';
-// // let ojb2 = 'teacher';
+// let job1='programmer';
+// let ojb2 = 'teacher';
 
-// // //////
+// //////
 
-// // let country = 'South Korea';
-// // let continent = 'Asia'
-// // let population = '50 millions'
+// let country = 'South Korea';
+// let continent = 'Asia'
+// let population = '50 millions'
 
-// // console.log(country)
-// // console.log(continent)
-// // console.log(population)
+// console.log(country)
+// console.log(continent)
+// console.log(population)
 
-// // let javascriptIsFun = true;
+// let javascriptIsFun = true;
 
-// // console.log(javascriptIsFun)
+// console.log(javascriptIsFun)
 
-// // console.log(typeof true)
-// // console.log(typeof javascriptIsFun)
-// // console.log(typeof 23)
-// // console.log(typeof 'Jonas')
+// console.log(typeof true)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'Jonas')
 
-// // javascriptIsFun="Yes"
-// // console.log(typeof javascriptIsFun)
+// javascriptIsFun="Yes"
+// console.log(typeof javascriptIsFun)
 
-// // let year;
-// // console.log(year)
-// // console.log(typeof year)
+// let year;
+// console.log(year)
+// console.log(typeof year)
 
-// // year=1994;
-// // console.log(year)
-// // console.log(typeof year)
-// // console.log(typeof null)
+// year=1994;
+// console.log(year)
+// console.log(typeof year)
+// console.log(typeof null)
 
 
 
@@ -77,13 +77,97 @@
 //   Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 // m tall.
 
-const marksWeights = 78
-const marksHeight = 1.69
-const johnWeights = 92
-const johnHeight = 1.95
-const markBMI= marksWeights/marksHeight **2
-const johnBMI=johnWeights/johnHeight **2
+// const marksWeights = 78
+// const marksHeight = 1.69
+// const johnWeights = 92
+// const johnHeight = 1.95
+// const markBMI= marksWeights/marksHeight **2
+// const johnBMI=johnWeights/johnHeight **2
 
-const markHigherBMI = markBMI>johnBMI
-console.log(markBMI,johnBMI)
-console.log(markHigherBMI)
+// const markHigherBMI = markBMI>johnBMI
+// console.log(markBMI,johnBMI)
+// console.log(markHigherBMI)
+
+// const firstName = 'Jonas '
+// const job='teacher'
+// const birthday = 1991
+
+// console.log(`${job}\n\
+// ${birthday}\n\
+//  nice
+// `)
+// console.log(`${job} ${birthday}
+// ${birthday}
+//  nice
+// `)
+
+// const age = 12;
+// const isOldEnough =age >= 18;
+
+// if (age >= 18){ 
+//     console.log( 'good')}
+// else{
+//     console.log('sorry')
+// }
+
+
+// const marksWeights = 48
+// const marksHeight = 1.69
+// const johnWeights = 92
+// const johnHeight = 1.95
+// const markBMI= marksWeights/marksHeight **2
+// const johnBMI=johnWeights/johnHeight **2
+
+// if(markBMI>johnBMI){
+// console.log(`Mark's BMI (${markBMI.toFixed(1)}) is higher than John's (${johnBMI.toFixed(1)})!`)
+
+// }else{
+//     console.log(`John's BMI(${johnBMI.toFixed(1)}) is higher than Mark's!(${markBMI.toFixed(1)})`)
+// }
+
+// let n='1'+1;
+// n = n-1;
+// console.log(n)
+
+// const hasDriverLicense = false
+// const hasGoodVision = false
+
+// console.log(hasDriverLicense&&hasGoodVision)
+// console.log(hasDriverLicense || hasGoodVision)
+// console.log(!hasDriverLicense)
+
+// const shouldDrive = hasGoodVision&&hasDriverLicense
+// const getVision = !hasGoodVision&&hasDriverLicense
+// if(shouldDrive){
+//     console.log('go to drive')
+// }else if(getVision){
+//     console.log('get glasses')
+// }else{
+//     console.log('do somethinf')
+// }
+
+// const isTired = true 
+
+// if(hasDriverLicense || hasGoodVision||isTired ){
+//     console.log('go to drive')
+// }else if(getVision){
+//     console.log('get glasses')
+// }else{
+//     console.log('do somethinf')
+// }
+
+
+const scoreDolphins = ((961+108+32)/3).toFixed(2) ;
+const scoreKoalas = ((961+108+32)/3).toFixed(2) ;
+console.log(scoreDolphins,scoreKoalas)
+
+if (scoreDolphins > scoreKoalas && scoreDolphins>=100 ){
+    console.log('Dolphins won!')
+}else if( scoreKoalas > scoreDolphins && scoreKoalas>=100){
+    console.log(' Koalas won')
+}else if(scoreKoalas === scoreDolphins&& scoreKoalas>=100){
+    console.log(' Draw')
+}else{
+    console.log('??')
+}
+
