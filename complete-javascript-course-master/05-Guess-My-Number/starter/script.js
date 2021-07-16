fcfc9d80633e8f9ad2c3638643265cfab1c98562
Martin Cho <zoveloper@gmail.com>
2021-07-15 21:46:17 -0400

    JS #75 Dom addeventListner, querySelector, value, textContent method

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+//document.querySelector
+// document.querySelector('.number').textContent = 33;
+// document.querySelector('.score').textContent = 122;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.message').textContent = 'testing ';
+// console.log(document.querySelector('.message').textContent);
+
+//
+
+document.querySelector('.check').addEventListener('click', () => {
+  console.log(document.querySelector('.guess').value);
+  const guess = document.querySelector('.guess').value;
+  console.log(typeof guess);
+});
