4b885b760e8dfc7c48e05a37ac256066e778da87
Martin Cho <zoveloper@gmail.com>
2021-07-20 22:06:36 -0400

    JS #82 manipulating css and key press handler

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -10,9 +10,43 @@
 // console.log(document.querySelector('.message').textContent);
 
 //
+let sNumber = Math.trunc(Math.random() * 5) + 1;
+
+let score = 25;
+
+document.querySelector('.score').textContent = score;
+
+document.querySelector('.again').addEventListener('click', () => {
+  sNumber = Math.trunc(Math.random() * 5) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+});
 
 document.querySelector('.check').addEventListener('click', () => {
   console.log(document.querySelector('.guess').value);
   const guess = document.querySelector('.guess').value;
+
   console.log(typeof guess);
+
+  console.log(typeof sNumber);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'no Number';
+  } else if (guess == sNumber) {
+    document.querySelector('.number').textContent = sNumber;
+    document.querySelector('.message').textContent = 'well done!';
+    document.querySelector('body').style.backgroundColor = 'pink';
+    document.querySelector('.number').style.width = '30rem';
+    console.log(document.querySelector('.message', guess));
+  } else if (guess < sNumber) {
+    document.querySelector('.message').textContent = 'up';
+    score--;
+    console.log(document.querySelector('.message', guess));
+  } else if (guess > sNumber) {
+    document.querySelector('.message').textContent = 'down';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+console.log(modal.classList);
+console.log(modal.style.display);
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked'[i]);
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
